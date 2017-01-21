978910a7c4bb570ed0376f714305dace9823dfd4
tsun1650 <tonysun996@gmail.com>
2017-01-21 13:52:44 -0500

    Adding Post Object
    Uploading Post

---
+++ MemeYak/app/src/main/java/com/example/tonys/memeyak/Model/Post.java
@@ -0,0 +1,25 @@
+package com.example.tonys.memeyak.Model;
+
+import android.media.Image;
+
+/**
+ * Created by tonys on 1/21/2017.
+ */
+public class Post {
+    int voteCount;
+    String imgSrc;
+    public Post () {
+        String src = "drawable/ic_action_name";
+        voteCount = 0;
+    }
+    public Post (String imgSrc, int vote) {
+        this.imgSrc = imgSrc;
+        this.voteCount = vote;
+    }
+    public void vote(int vote) {
+        voteCount += vote;
+    }
+    public int getVoteCount() {return voteCount;}
+    public void setImgSrc(String s) {imgSrc = s;}
+    public String getImgSrc() {return imgSrc;}
+}
